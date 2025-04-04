f32a6007479f828f0842918a0824df4cf87efefa
abbasmirza12 <abbas.mirza12@outlook.com>
2025-04-04 20:04:33 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
+// Highlight current page in nav
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll("nav a");
+  const currentUrl = window.location.pathname.split("/").pop();
+
+  navLinks.forEach(link => {
+    if (link.getAttribute("href") === currentUrl) {
+      link.classList.add("active");
+    }
+  });
+});
 
